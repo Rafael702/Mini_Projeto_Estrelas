7e969822b9d4f13a4df06d5f429669b8b22d43af
Anna Karolyne <dev.akvc@gmail.com>
2021-09-30 12:34:43 -0300

    Classe Cardápio pré-modelada

--- src/br/com/zup/Cardapio/Cardapio.java
+++ src/br/com/zup/Cardapio/Cardapio.java
@@ -1,4 +1,12 @@
 package src.br.com.zup.Cardapio;
 
 public class Cardapio {
+
+    // Método construtor
+    public Cardapio() {
+    }
+
+    // Métodos getters e setters
+
+    // Método toString
 }
