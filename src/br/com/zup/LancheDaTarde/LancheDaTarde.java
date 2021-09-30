1628b0befdbf61882bf82fec64c84cd86bac3523
Anna Karolyne <dev.akvc@gmail.com>
2021-09-30 14:09:59 -0300

    Classe Lanche da tarde - Métodos construtores, getters e setters

--- src/br/com/zup/LancheDaTarde/LancheDaTarde.java
+++ src/br/com/zup/LancheDaTarde/LancheDaTarde.java
@@ -1,4 +1,36 @@
 package src.br.com.zup.LancheDaTarde;
 
-public class LancheDaTarde {
+public class LancheDaTarde extends br.com.zup.RefeicoesDiarias.RefeicoesDiarias {
+
+    // Método construtor
+    public LancheDaTarde(String nomeDoAlimento, double qtdDeCaloria) {
+        super(nomeDoAlimento, qtdDeCaloria);
+    }
+
+    // Métodos getters e setters
+    @Override
+    public String getNomeDoAlimento() {
+        return super.getNomeDoAlimento();
+    }
+
+    @Override
+    public void setNomeDoAlimento(String nomeDoAlimento) {
+        super.setNomeDoAlimento(nomeDoAlimento);
+    }
+
+    @Override
+    public double getQtdDeCaloria() {
+        return super.getQtdDeCaloria();
+    }
+
+    @Override
+    public void setQtdDeCaloria(double qtdDeCaloria) {
+        super.setQtdDeCaloria(qtdDeCaloria);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
 }
