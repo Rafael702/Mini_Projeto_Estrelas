f9ae4fbb04f9ee18f41e69a653fc2856068b5616
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-30 17:02:16 -0300

    Testes.

--- src/br/com/zup/Sistema/Sistema.java
+++ src/br/com/zup/Sistema/Sistema.java
@@ -2,4 +2,10 @@ package br.com.zup.Sistema;
 
 public class Sistema {
 
+    public static String alteraString(String mensagem){
+        String string = mensagem;
+        return string;
+    }
+
+
 }
